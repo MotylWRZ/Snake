2a3ac3ce312301974b7bb517356b77c3382339a7
MotylWRZ <motyl9635@gmail.com>
2020-06-26 02:40:30 +0100

    - Added basic functions in SinglyLinkedList class - These are PushBack() and PushFront() (both not finished) and Lenght() (finished) - Added head and tail pointers as private class members Signed-off-by: MotylWRZ <motyl9635@gmail.com>

--- Snake/SinglyLinkedList.cpp
+++ Snake/SinglyLinkedList.cpp
@@ -10,3 +10,25 @@ SinglyLinkedList::SinglyLinkedList()
 SinglyLinkedList::~SinglyLinkedList()
 {
 }
+
+void SinglyLinkedList::PushFront(Node* pHead, int pData)
+{
+
+}
+
+void SinglyLinkedList::PushBack(Node* pHead, int pData)
+{
+
+}
+
+int SinglyLinkedList::Length(Node* pHead)
+{
+	Node* currentNodePtr = pHead; // current node ptr
+	int count = 0;
+	while (currentNodePtr != nullptr)
+	{
+		count++;
+		currentNodePtr = currentNodePtr->next; // point to the next node
+	}
+	return count;
+}
--- Snake/SinglyLinkedList.h
+++ Snake/SinglyLinkedList.h
@@ -14,5 +14,13 @@ class SinglyLinkedList
 public:
 	SinglyLinkedList();
 	virtual ~SinglyLinkedList();
+
+	void PushFront(Node* pHead, int pData); // push a new node at the front of the list
+	void PushBack(Node* pHead, int pData); // push a new node at the end of the list
+	int Length(Node* pHead); // get the lenght of the linked list
+
+private:
+	Node* m_head; // Head (first node)
+	Node* m_tail; // Tail (the last node)
 };
 
