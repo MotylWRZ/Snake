409f2731c78e97f0a3a953e43bb5fb7cffbe5fd5
MotylWRZ <motyl9635@gmail.com>
2020-06-27 23:26:25 +0100

    - Singly Linked class has been extended - Some getter have ben added to retrieve the head an dthe last element of the list Signed-off-by: MotylWRZ <motyl9635@gmail.com>

--- Snake/SinglyLinkedList.h
+++ Snake/SinglyLinkedList.h
@@ -20,9 +20,15 @@ public:
 	void PushBack(T pData); // push a new node at the end of the list
 	int Length(); // get the lenght of the linked list
 
+	//Accesors/Mutators
+	Node<T>* GetHeadPtr() { return m_head; }; // Return a pointer to the head node
+	Node<T>* GetEndPtr() { return m_end; }; //Return a pointer to the end node
+
 private:
 	Node<T>* m_head; // Head (first node)
 	Node<T>* m_end; // End (the last node)
+	
+
 
 };
 
