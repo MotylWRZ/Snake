8968287c4e52796c1c61a7c373db124e95f6b8de
MotylWRZ <motyl9635@gmail.com>
2020-07-03 21:34:24 +0100

    - Added contructor and class members for UITextElement class Signed-off-by: MotylWRZ <motyl9635@gmail.com>

--- Snake/UITextElement.cpp
+++ Snake/UITextElement.cpp
@@ -6,6 +6,17 @@ UITextElement::UITextElement()
 {
 }
 
+UITextElement::UITextElement(std::string pText, int pFontSize, sf::Color pTextColor, sf::Vector2f pPosition)
+	:m_textString(pText)
+	, m_fontSize(pFontSize)
+	, m_textColor(pTextColor)
+	, m_position(pPosition)
+{
+	m_text.setString(m_textString);
+	m_text.setCharacterSize(m_fontSize);
+	m_text.setFillColor(m_textColor);
+	m_text.setPosition(m_position);
+}
 
 UITextElement::~UITextElement()
 {
--- Snake/UITextElement.h
+++ Snake/UITextElement.h
@@ -1,8 +1,19 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+#include "UITextElement.h"
+#include <string>
 class UITextElement
 {
 public:
 	UITextElement();
+	UITextElement(std::string pText, int pFontSize, sf::Color pTextColor, sf::Vector2f pPosition);
 	~UITextElement();
+
+private:
+	sf::Text  m_text;
+	std::string m_textString;
+	int		  m_fontSize;
+	sf::Color m_textColor;
+	sf::Vector2f m_position;
 };
 
